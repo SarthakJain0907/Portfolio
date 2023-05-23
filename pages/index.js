e47ddbbb1bcee3a06f58753c532ddae5012e0610
SarthakJain0907 <66311654+SarthakJain0907@users.noreply.github.com>
2023-05-23 10:08:37 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Layout, { siteTitle } from '/components/layout';
+import utilStyles from '/styles/utils.module.css';
 import Link from 'next/link'
-import Navbar from '../parts/navbar.js';
+import Navbar from '/parts/navbar.js';
 
 export default function Home() {
   return (
