57d3105e908827e12cfb72afaf21fb6fec0da3dc
SarthakJain0907 <66311654+SarthakJain0907@users.noreply.github.com>
2023-05-23 10:11:50 +0530

    Add files via upload

---
+++ parts/Navbar.js
@@ -0,0 +1,15 @@
+import stylesNav from '/styles/Navbar.module.css';
+import Link from 'next/link';
+
+
+export default function Navbar() {
+    return(
+        <div className={stylesNav.navbar}>
+            <div className={stylesNav.card}><Link href='/' className={stylesNav.navLinks}>Home</Link></div>
+            <div className={stylesNav.card}><Link href='../links/coding' className={stylesNav.navLinks}>Coding</Link></div>
+            <div className={stylesNav.card}><Link href='../links/work' className={stylesNav.navLinks}>Work</Link></div>
+            <div className={stylesNav.card}><Link href='../links/contact' className={stylesNav.navLinks}>Contact Me</Link></div>
+        </div>
+
+    )
+}
