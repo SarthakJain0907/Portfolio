4e3f22899db0d5a51ccbe279b5d0c436ad27e771
SarthakJain0907 <66311654+SarthakJain0907@users.noreply.github.com>
2023-05-24 19:10:01 +0530

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -26,7 +26,7 @@ export default function Layout({ children, home }) {
           <Navbar/>
             <Image
               priority
-              src="/pages/links/profile.jpg"
+              src="/profile.jpg"
               height={144}
               width={144}
               alt=""
