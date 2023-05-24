a3cbfe48a684a09b9293783af4e449f010b3cbcc
SarthakJain0907 <66311654+SarthakJain0907@users.noreply.github.com>
2023-05-24 12:42:30 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>I am interested in Mathematics, Web Development, tennis, and pretty much anything that comes my way.</p>
+        <p>The code for this website is on github: <Link href="https://github.com/SarthakJain0907/portfolio/tree/main">here</Link></p>
       </section>
     </Layout>
   );
